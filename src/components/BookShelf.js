bdd1257bfdd9a1d592f0fec7e69a2f258bf110d4
M-Bohram <mostafabohram@gmail.com>
2020-11-20 05:10:35 +0200

    feat: Change App component to small components

---
+++ src/components/BookShelf.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Book from "./Book";
+
+class BookShelf extends Component {
+  render() {
+    const { books } = this.props;
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li>
+                <Book
+                  imageUrl={book.imageUrl}
+                  title={book.title}
+                  author={book.author}
+                />
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookShelf;
